569ce9f2be3f725f43663c8e18450f8b3dccd71b
wontonwindmill <winstonkhackett@gmail.com>
2024-04-01 01:21:48 -0400

    mobile friednly

--- fish-spots/src/components/MainPosts.js
+++ fish-spots/src/components/MainPosts.js
@@ -2,11 +2,11 @@ import {
     DownOutlined
 } from '@ant-design/icons';
 
-import { Card, Space, Image, Row, Col, Typography, Tag, Dropdown } from 'antd';
+import { Card, Space, Image, Row, Col, Typography, Tag, Dropdown, Flex } from 'antd';
 
 const { Text } = Typography;
 
-export default function Posts(props){
+export default function MainPosts(props){
     return (
           <Space direction="vertical"  style={{ display: 'flex' }}>
             <h2>Find your favorite fishing spots!</h2>
@@ -42,38 +42,56 @@ export default function Posts(props){
               <Card 
                 className="card" 
                 size="small"
-                title={<div><p>{item.title}</p><Text type='secondary'>{item.date_posted}</Text></div>} 
+                title={<div><p>{item.title}</p> <Text type='secondary'>{item.date_posted}</Text></div>} 
                 description={item.date_posted} 
                 extra=
                   {
                     <a onClick={(e) => {e.preventDefault(); props.addFavorite(item);}} href="#">Save</a>
                   } 
-                >         
-                <Row gutter={16}>
-                  <Col className="gutter-row" span={8}>
+                >
+                {props.isMobile ? (
+                  <div>
+                    <Flex direction="horizontal">
                       <div>
-                        <Image
-                          src={item.image}
-                        />
-                        <Text type="secondary">{"("+item.image_credit+")"}</Text>
-                      </div>
-                      <div>
-                        <h4>Fish Species: </h4>
-                        {item.fish_types.map((type) => <Tag color="volcano">{type}</Tag>)}
-                      </div>            
-                  </Col>
-                  <Col className="gutter-row" span={16}>
+                          <Image
+                            src={item.image}
+                          />
+                          <Text type="secondary">{"("+item.image_credit+")"}</Text>
+                        </div>     
+                    </Flex>
                     <p className="description">{item.description}</p>
                     <h4>Locations: </h4>
                     {item.locations.map((location) => <Tag color="geekblue">{location}</Tag>)}
                     <h4>Months:</h4>
                     {item.months.map((month) => <Tag color="green">{month}</Tag>)}
-                  </Col>
-                </Row>
+                    <h4>Fish Species: </h4>
+                          {item.fish_types.map((type) => <Tag color="volcano">{type}</Tag>)}
+                  </div>
+                ) : (
+                  <Row gutter={16}>
+                    <Col className="card-image-column" span={8}>
+                        <div>
+                          <Image
+                            src={item.image}
+                          />
+                          <Text type="secondary">{"("+item.image_credit+")"}</Text>
+                        </div>
+                        <div>
+                          <h4>Fish Species: </h4>
+                          {item.fish_types.map((type) => <Tag color="volcano">{type}</Tag>)}
+                        </div>            
+                    </Col>
+                    <Col span={16}>
+                      <p className="description">{item.description}</p>
+                      <h4>Locations: </h4>
+                      {item.locations.map((location) => <Tag color="geekblue">{location}</Tag>)}
+                      <h4>Months:</h4>
+                      {item.months.map((month) => <Tag color="green">{month}</Tag>)}
+                    </Col>
+                  </Row>
+                )}         
               </Card>
             ))}
           </Space>
-        
     )
-
 }
